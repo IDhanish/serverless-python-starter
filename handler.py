107df958d7bce347ff6c0185b968d6f8f32e74ac
IDhanish <87757749+IDhanish@users.noreply.github.com>
2023-10-30 17:36:52 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,7 +1,64 @@
 import json
+import os
+from mysql.connector import connect
+from mysql import connector
 
-def hello(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps("Hello, world!"),
-    }
+
+def GetConnection():
+    conn = None
+    conn = connector.connect(
+        host=os.environ.get("TLOG_DB_HOST"),
+        user=os.environ.get("TLOG_DB_USER"),
+        passwd=os.environ.get("TLOG_DB_PASS"),
+        port=3306,
+        db="tlog_dashboard_db",
+    )
+    return conn
+
+
+def CacheCleanup(Date):
+    proc = ""
+    result = []
+
+    proc = "tlog_dashboard_db."
+    args = Date
+
+    result = executeGet(proc, args, cursor)
+    return result
+
+
+def executeGet(proc, args, cursor):
+    try:
+        result = []
+        cursor.callproc(proc, args)
+        for result in cursor.stored_results():
+            dataset = result.fetchall()
+            columns = result.description
+        result = []
+        for value in dataset:
+            tmp = {}
+            for index, column in enumerate(value):
+                tmp[columns[index][0]] = column
+            result.append(tmp)
+
+    except connector.Error as e:
+        result = e
+
+    return result
+
+
+def main(event, context):
+    conn = GetConnection()
+    cursor = conn.cursor()
+    Result = []
+    GrossSales = []
+    HourlySales = []
+
+    StatusMessage = CacheCleanup(Date)
+
+    Result.append({"StatusMessage": StatusMessage})
+
+    conn.close()
+
+    # TODO implement
+    return {"StatusCode": 200, "Result": Result}
